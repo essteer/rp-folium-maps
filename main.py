df9820e64e13c7df2a0ecd5585be612c5096b2c0
essteer <essteer@pm.me>
2023-03-11 10:13:08 +0800

    add basic choropleth features

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import folium
+import pandas as pd
 
+eco_footprints = pd.read_csv("footprint.csv")
 political_countries_url = (
     "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
 )
 
 m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
-folium.GeoJson(political_countries_url).add_to(m)
+folium.Choropleth(
+    geo_data=political_countries_url,
+    data=eco_footprints,
+    columns=["Country/region", "Ecological footprint"],
+    key_on="feature.properties.name",
+).add_to(m)
 
 m.save("footprint.html")
